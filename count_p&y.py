428cfc536fad686704822e9af7906e9f9ab13c59
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-26 22:41:24 +0900

    commit

--- count_p&y.py
+++ count_p&y.py
@@ -1,16 +1,18 @@
 s = "pPooyY"
 
-def solution(s):
-    p_count = 0
-    y_count = 0
-    for x in s:
-        if x == 'p' or x == 'P':
-            p_count += 1
-        elif x == 'y' or x == 'Y':
-            y_count += 1
+print(s.count('P'))
 
-    if p_count == y_count:
-        return True
-    else :
-        return False
-print(solution(s))
+# def solution(s):
+#     p_count = 0
+#     y_count = 0
+#     for x in s:
+#         if x == 'p' or x == 'P':
+#             p_count += 1
+#         elif x == 'y' or x == 'Y':
+#             y_count += 1
+
+#     if p_count == y_count:
+#         return True
+#     else :
+#         return False
+# print(solution(s))
