b100881fc17168f5d4f61a96d17efe5053cda588
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-28 16:01:22 +0900

    count solution

--- count_p&y.py
+++ count_p&y.py
@@ -1,18 +1,16 @@
 s = "pPooyY"
 
-print(s.count('P'))
+def solution(s):
+    p_count = 0
+    y_count = 0
+    for x in s:
+        if x == 'p' or x == 'P':
+            p_count += 1
+        elif x == 'y' or x == 'Y':
+            y_count += 1
 
-# def solution(s):
-#     p_count = 0
-#     y_count = 0
-#     for x in s:
-#         if x == 'p' or x == 'P':
-#             p_count += 1
-#         elif x == 'y' or x == 'Y':
-#             y_count += 1
-
-#     if p_count == y_count:
-#         return True
-#     else :
-#         return False
-# print(solution(s))
+    if p_count == y_count:
+        return True
+    else :
+        return False
+print(solution(s))
